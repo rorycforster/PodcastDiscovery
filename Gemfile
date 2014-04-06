c66d2b366a11fa3ba9ac23fc113643763381e038
Rory Budnick <rorybudnick@gmail.com>
2014-04-06 13:49:40 -0400

    get podcast description using nokogiri and open uri to scrape site

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'httparty'
 gem 'json'
+gem 'nokogiri'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'httparty'
+
 class PodcastsController < ApplicationController
   def index
      results_httparty = HTTParty.get("https://itunes.apple.com/search?entity=podcast&limit=25&term=#{params[:search_term]}")
@@ -5,8 +9,13 @@ class PodcastsController < ApplicationController
     # genre_results = HTTParty.get(https://itunes.apple.com/search?entity=podcast&limit=25&genreIndex=#{params[:search_term]})
   end
   def info
-    info_httparty = HTTParty.get()
-
+    info_httparty = HTTParty.get("https://itunes.apple.com/lookup?id=#{params[:podcast_id]}")
+    @info_results_json = JSON.parse(info_httparty)["results"][0]
+    @image = @info_results_json["artworkUrl600"]
+    @author = @info_results_json["artistName"]
+    url_to_scrape_for_description = @info_results_json["collectionViewUrl"]
+    @doc = Nokogiri::HTML(open(url_to_scrape_for_description))
+    @description = @doc.css("div.product-review p")
   end
   def search
   end
