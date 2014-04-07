4a40b3507a7eaa6b0e03afc573c8a95a31881ee2
Rory Budnick <rorybudnick@gmail.com>
2014-04-07 12:52:52 -0400

    changed application helper for page_entries_info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def page_entries_info(collection, options = {})
+  entry_name = options[:entry_name] || (collection.empty?? 'item' :
+      collection.first.class.name.split('::').last.titleize)
+  if collection.total_pages < 2
+    case collection.size
+    when 0; "No #{entry_name.pluralize} found"
+    else; "Displaying all results"
+    end
+  else
+    %{Displaying %d - %d of %d results} % [
+      collection.offset + 1,
+      collection.offset + collection.length,
+      collection.total_entries
+    ]
+  end
+end
 end
